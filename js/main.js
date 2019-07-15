2da36f21e1dd8117f81638d26b28802be4c08a02
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-07-15 08:00:51 +0300

    fix javascript errors trial 1

--- js/main.js
+++ js/main.js
@@ -1,59 +1,72 @@
+
 function generateName() {
   var date = document.getElementById("numDD");
   var month = document.getElementById("numMM");
   var century = document.getElementById("numCC");
   var year = document.getElementById("numYY");
   var gender = document.getElementById("txtgender");
-  var DD= parseInt(day);
-  var MM= parseInt(month);
-  var CC= parseInt(century);
-  var YY= parseInt(year);
-  var dayBorn = ((((((CC/4) -((2*CC)-1)) + ((5*YY)/4) + (26*((MM+1)/10))) + DD)) % 7);
-  alert("You were born on " + dayBorn + "!");
+}
+  // var DD= parseInt(date);
+  // var MM= parseInt(month);
+  // var CC= parseInt(century);
+  // var YY= parseInt(year);
+function dayName(){
+  generateName();
+  var dayBorn = ( ( (century/4) -2*year-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
+  // var akanDate = parseInt(dayBorn);
+  // alert(akanDate);
+  console.log(dayBorn);
+  return (math.floor(dayBorn));
 }
 
-if (dayBorn == 0 && gender == "m") {
-  console.log("Kwasi");
+// document.getElementById("generateName").onclick = generateName(DD,MM,CC,YY);
+function checkGender(){
+if (dayBorn === 0 && gender == "m") {
+  alert("Kwasi");
 }
-else if (dayborn == 0 && gender == "f"){
-  console.log("Akosua");
+else if (dayBorn === 0 && gender == "f"){
+  alert("Akosua");
 }
-else if (dayBorn == 1 && gender == "m"){
-  console.log("Kwadwo");
+else if (dayBorn === 1 && gender == "m"){
+  alert("Kwadwo");
 }
-else if (dayborn == 1 && gender == "f"){
-  console.log("Adwoa");
+else if (dayBorn === 1 && gender == "f"){
+  alert("Adwoa");
 }
-else if (dayBorn == 2 && gender == "m"){
-  console.log("Kwabena");
+else if (dayBorn === 2 && gender == "m"){
+  alert("Kwabena");
 }
-else if (dayborn == 2 && gender == "f"){
-  console.log("Abenaa");
+else if (dayBorn === 2 && gender == "f"){
+  alert("Abenaa");
 }
-else if (dayBorn == 3 && gender == "m"){
-  console.log("Kwaku");
+else if (dayBorn === 3 && gender == "m"){
+  alert("Kwaku");
 }
-else if (dayborn == 3 && gender == "f"){
-  console.log("Akua");
+else if (dayBorn === 3 && gender == "f"){
+  alert("Akua");
 }
 else if (dayBorn == 4 && gender == "m"){
-  console.log("Yaw");
+  alert("Yaw");
 }
-else if (dayborn == 4 && gender == "f"){
-  console.log("Yaa");
+else if (dayBorn === 4 && gender == "f"){
+  alert("Yaa");
 }
-else if (dayBorn == 5 && gender == "m"){
-  console.log("Kofi");
+else if (dayBorn === 5 && gender == "m"){
+  alert("Kofi");
 }
-else if (dayborn == 5 && gender == "f"){
-  console.log("Afua");
+else if (dayBorn === 5 && gender == "f"){
+  alert("Afua");
 }
-else if (dayBorn == 6 && gender == "m"){
-  console.log("Kwame");
+else if (dayBorn === 6 && gender == "m"){
+  alert("Kwame");
 }
-else if (dayborn == 6 && gender == "f"){
-  console.log("Ama");
+else if (dayBorn === 6 && gender == "f"){
+  alert("Ama");
 }
 else{
   alert("INPUT IS INVALID! PLEASE TRY AGAIN");
+}
+}
+function akanName(){
+  
 }
