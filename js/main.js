8d742bfc1a9a39335eea5cbbaf556049f3047110
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-07-15 12:48:14 +0300

    add toLowerCase on gender field

--- js/main.js
+++ js/main.js
@@ -8,46 +8,46 @@ function generateName(myForm) {
   var result = (((( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7))+2).toFixed();
   var name = document.getElementById("txtfullname");
 
-if (result < 1 && gender == "m") {
+if (result < 1 && gender.toLowerCase() == "m") {
   alert("Hi! Your Akan name is  Kwasi");
 }
-else if (result < 1 && gender == "f"){
+else if (result < 1 && gender.toLowerCase() == "f"){
   alert("Hi! Your Akan name is  Akosua");
 }
-else if (result < 2 && gender == "m"){
+else if (result < 2 && gender.toLowerCase() == "m"){
   alert("Hi! Your Akan name is Kwadwo");
 }
-else if (result < 2 && gender == "f"){
+else if (result < 2 && gender.toLowerCase() == "f"){
   alert("Hi! Your Akan name is  Adwoa");
 }
-else if (result < 3 && gender == "m"){
+else if (result < 3 && gender.toLowerCase() == "m"){
   alert("Hi! Your Akan name is  Kwabena");
 }
-else if (result < 3 && gender == "f"){
+else if (result < 3 && gender.toLowerCase() == "f"){
   alert("Hi! Your Akan name is  Abenaa");
 }
-else if (result < 4 && gender == "m"){
+else if (result < 4 && gender.toLowerCase() == "m"){
   alert("Hi! Your Akan name is  Kwaku");
 }
-else if (result < 4 && gender == "f"){
+else if (result < 4 && gender.toLowerCase() == "f"){
   alert("Hi! Your Akan name is  Akua");
 }
-else if (result < 5 && gender == "m"){
+else if (result < 5 && gender.toLowerCase() == "m"){
   alert("Hi! Your Akan name is  Yaw");
 }
-else if (result < 5 && gender == "f"){
+else if (result < 5 && gender.toLowerCase() == "f"){
   alert("Hi! Your Akan name is  Yaa");
 }
-else if (result < 6 && gender == "m"){
+else if (result < 6 && gender.toLowerCase() == "m"){
   alert("Hi! Your Akan name is  Kofi");
 }
-else if (result < 6 && gender == "f"){
+else if (result < 6 && gender.toLowerCase() == "f"){
   alert("Hi! Your Akan name is  Afua");
 }
-else if (result < 7 && gender == "m"){
+else if (result < 7 && gender.toLowerCase() == "m"){
   alert("Hi! Your Akan name is  Kwame");
 }
-else if (result < 7 && gender == "f"){
+else if (result < 7 && gender.toLowerCase() == "f"){
   alert("Hi! Your Akan name is  Ama");
 }
 else{
