9dff9619a3d083056d0b081f6893b8e01a930530
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-07-15 05:07:36 +0300

    add output text function

--- js/main.js
+++ js/main.js
@@ -2,5 +2,12 @@ function generateName() {
   var date = newDate(DD, MM, CCYY);
   var DD = getDate();
   var MM = getMonth();
-  var CC = 
+  var CC = document.getElementById("CC");
+  var YY = document.getElementById("YY");
+  var gender = document.getElementById("gender");
   var dayBorn=(((((CC/4) -((2*CC)-1)) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD)) % 7)
+}
+function outputText(){
+  var fullName = document.getElementById("fullname");
+  var akanName = document.getElementById("akanName");
+  alert("Hi there, " + fullName + "!. Your Akan name is," + akanName + "  :)")
