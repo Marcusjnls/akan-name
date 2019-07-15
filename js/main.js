ee7ac3667b79a7fa4d2b052c81fb551e4eb225c1
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-07-15 11:44:20 +0300

    fix errors in functions

--- js/main.js
+++ js/main.js
@@ -1,72 +1,57 @@
 
-function generateName() {
-  var date = document.getElementById("numDD");
-  var month = document.getElementById("numMM");
-  var century = document.getElementById("numCC");
-  var year = document.getElementById("numYY");
-  var gender = document.getElementById("txtgender");
-}
-  // var DD= parseInt(date);
-  // var MM= parseInt(month);
-  // var CC= parseInt(century);
-  // var YY= parseInt(year);
-function dayName(){
-  generateName();
-  var dayBorn = ( ( (century/4) -2*year-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
-  // var akanDate = parseInt(dayBorn);
-  // alert(akanDate);
-  console.log(dayBorn);
-  return (math.floor(dayBorn));
-}
+function generateName(myForm) {
+  var DD = document.getElementById("numDD").value;
+  var MM = document.getElementById("numMM").value;
+  var CC = document.getElementById("numCC").value;
+  var YY = document.getElementById("numYY").value;
+  var gender = document.getElementById("txtgender").value;
+  var result = (((( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7))+2).toFixed();
+  var name = document.getElementById("txtfullname");
 
-// document.getElementById("generateName").onclick = generateName(DD,MM,CC,YY);
-function checkGender(){
-if (dayBorn === 0 && gender == "m") {
-  alert("Kwasi");
+if (result < 1 && gender == "m") {
+  alert("Hi! Your Akan name is  Kwasi");
 }
-else if (dayBorn === 0 && gender == "f"){
-  alert("Akosua");
+else if (result < 1 && gender == "f"){
+  alert("Hi! Your Akan name is  Akosua");
 }
-else if (dayBorn === 1 && gender == "m"){
-  alert("Kwadwo");
+else if (result < 2 && gender == "m"){
+  alert("Hi! Your Akan name is Kwadwo");
 }
-else if (dayBorn === 1 && gender == "f"){
-  alert("Adwoa");
+else if (result < 2 && gender == "f"){
+  alert("Hi! Your Akan name is  Adwoa");
 }
-else if (dayBorn === 2 && gender == "m"){
-  alert("Kwabena");
+else if (result < 3 && gender == "m"){
+  alert("Hi! Your Akan name is  Kwabena");
 }
-else if (dayBorn === 2 && gender == "f"){
-  alert("Abenaa");
+else if (result < 3 && gender == "f"){
+  alert("Hi! Your Akan name is  Abenaa");
 }
-else if (dayBorn === 3 && gender == "m"){
-  alert("Kwaku");
+else if (result < 4 && gender == "m"){
+  alert("Hi! Your Akan name is  Kwaku");
 }
-else if (dayBorn === 3 && gender == "f"){
-  alert("Akua");
+else if (result < 4 && gender == "f"){
+  alert("Hi! Your Akan name is  Akua");
 }
-else if (dayBorn == 4 && gender == "m"){
-  alert("Yaw");
+else if (result < 5 && gender == "m"){
+  alert("Hi! Your Akan name is  Yaw");
 }
-else if (dayBorn === 4 && gender == "f"){
-  alert("Yaa");
+else if (result < 5 && gender == "f"){
+  alert("Hi! Your Akan name is  Yaa");
 }
-else if (dayBorn === 5 && gender == "m"){
-  alert("Kofi");
+else if (result < 6 && gender == "m"){
+  alert("Hi! Your Akan name is  Kofi");
 }
-else if (dayBorn === 5 && gender == "f"){
-  alert("Afua");
+else if (result < 6 && gender == "f"){
+  alert("Hi! Your Akan name is  Afua");
 }
-else if (dayBorn === 6 && gender == "m"){
-  alert("Kwame");
+else if (result < 7 && gender == "m"){
+  alert("Hi! Your Akan name is  Kwame");
 }
-else if (dayBorn === 6 && gender == "f"){
-  alert("Ama");
+else if (result < 7 && gender == "f"){
+  alert("Hi! Your Akan name is  Ama");
 }
 else{
-  alert("INPUT IS INVALID! PLEASE TRY AGAIN");
+  alert("INVALID!");
 }
 }
-function akanName(){
-  
-}
+generateName()
